213206c85541f9b2b8f2076cce571cb04469b992
Tolga <github.com.police355@passmail.net>
2026-01-14 20:23:10 +0100

    Update covid.py

--- scripts/sources/covid.py
+++ scripts/sources/covid.py
@@ -1,1 +1,9 @@
-
+def build():
+    ...
+    return {
+        "id": "covid",
+        "label": "COVID-19 (RKI, Landkreis)",
+        "metrics": ["incidence_7d", "cases_7d", "trend_pct"],
+        "latest": latest_out,
+        "timeseries": series_out
+    }
